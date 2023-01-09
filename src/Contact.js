1a6dc185c1073f858442a703d268febd310bb22b
Muhammad Usman <86666190+usmanmalik10@users.noreply.github.com>
2023-01-09 17:32:31 +0500

    contact page added

--- src/Contact.js
+++ src/Contact.js
@@ -33,7 +33,49 @@ const Contact = () => {
     }
   `;
 
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Contact page</h2>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d251.00689290534595!2d74.18444005376683!3d32.17369499176805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391f298fb0850659%3A0xa8f58b71cd205618!2sITS%20Tech%20World!5e0!3m2!1sen!2s!4v1673266037824!5m2!1sen!2s"
+        width="100%"
+        height="400"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        title="map"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+      <div className="container">
+        <div className="contact-form">
+          <form action="https://formspree.io/f/meqwqvgq" method="POST" className="contact-inputs">
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              reuired
+              autoComplete="off"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="Email"
+              reuired
+              autoComplete="off"
+            />
+            <textarea
+              name="Message"
+              cols="10"
+              rows="10"
+              placeholder="Enter your message"
+              autoComplete="off"
+            ></textarea>
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Contact;
