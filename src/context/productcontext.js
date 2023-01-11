54ed377815beefb49962bbc24608bbcd5e272fcb
Muhammad Usman <86666190+usmanmalik10@users.noreply.github.com>
2023-01-11 17:06:26 +0500

    axios install and API get

--- src/context/productcontext.js
+++ src/context/productcontext.js
@@ -1,19 +1,40 @@
-// create a context 
-// provider 
-// consumer => useContext Hook 
-import { createContext, useContext } from "react";
-
+// create a context
+// provider
+// consumer => useContext Hook
+import { createContext, useContext, useEffect } from "react";
+import axios from "axios";
 const AppContext = createContext();
 
+const API = "https://api.pujakaitem.com/api/products";
+
 const AppProvider = ({ children }) => {
-    return <AppContext.Provider value={{myName: "Usman Malik"}}> {children} </AppContext.Provider>
 
+    const getProducts = async (url) => {
+        const res = await axios.get(url);
+        const products = await res.data;
+        console.log( "Get Product", products);
+    };
+
+
+    useEffect(() => {
+        getProducts(API);
+      }, []);
+
+
+
+  return (
+    <AppContext.Provider value={{ myName: "Usman Malik" }}>
+      
+      {children}
+    </AppContext.Provider>
+  );
 };
 
-// Custom Hooks 
+
+
+// Custom Hooks
 const useProductContext = () => {
-    return useContext(AppContext);
+  return useContext(AppContext);
 };
-
 
 export { AppProvider, AppContext, useProductContext };
