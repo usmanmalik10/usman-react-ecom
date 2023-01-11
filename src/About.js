da4e17a9b52ec9129177880df658f79cfe33e0fa
Muhammad Usman <86666190+usmanmalik10@users.noreply.github.com>
2023-01-11 16:14:07 +0500

    context api

--- src/About.js
+++ src/About.js
@@ -1,11 +1,21 @@
-import React from "react";
+
 import HeroSection from "./components/HeroSection";
+import { useProductContext } from "./context/productcontext";
 
 const About = () => {
+
+  const {myName} = useProductContext();
+
     const data = {
       name: "FABAU Ecommerce ",
     };
-  return <HeroSection myData={data} />;
+  return ( 
+    <>
+    {myName}
+      <HeroSection myData={data} />
+
+    </>
+  ) ;
 };
 
 export default About;
