246028112c9d188fe39deaecd32d23bd721368a0
Muhammad Usman <86666190+usmanmalik10@users.noreply.github.com>
2023-01-13 17:57:17 +0500

    commit

--- src/components/MyImage.js
+++ src/components/MyImage.js
@@ -7,26 +7,27 @@ const MyImage = ({ imgs = [{ url: "" }] }) => {
 
   return (
     <Wrapper>
-      <div className="grid grid-four-column">
-        {imgs.map((curElm, index) => {
+     <div className="grid grid-four-column">
+      {
+        imgs.map((curElm, index) => {
           return (
             <figure>
               <img
-                src={curElm.url}
-                alt={curElm.filename}
-                className="box-image--style"
-                key={index}
-                onClick={() => setMainImage(curElm)}
+              src={curElm.url}
+              alt={curElm.filename}
+              className="box-image--style"
+              key={index}
+              onClick={() => setMainImage(curElm)}
               />
             </figure>
-          );
-        })}
-      </div>
-      {/* 2nd column  */}
-
-      <div className="main-screen">
-        <img src={mainImage.url} alt={mainImage.filename} />
-      </div>
+          )
+        })
+      }
+     </div>
+     {/* 2nd column  */}
+     <div className="main-screen">
+      <img src={mainImage.url} alt={mainImage.filename} />
+     </div>
     </Wrapper>
   );
 };
