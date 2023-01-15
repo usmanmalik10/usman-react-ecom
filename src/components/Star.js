360b4a09fd1699c395d89fb14ff64af3431eb0bf
Muhammad Usman <86666190+usmanmalik10@users.noreply.github.com>
2023-01-15 22:33:05 +0500

    commit

--- src/components/Star.js
+++ src/components/Star.js
@@ -2,6 +2,8 @@ import { FaStar, FaStarHalfAlt } from "react-icons/fa";
 import { AiOutlineStar } from "react-icons/ai";
 import styled from "styled-components";
 
+
+// Star Rating 
 const Star = ({ stars, reviews }) => {
   const ratingStar = Array.from({ length: 5 }, (elem, index) => {
     let number = index + 0.5;
