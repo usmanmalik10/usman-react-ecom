dc7e102ada9b1ebf2c89580db36cde8458fcfce6
Muhammad Usman <86666190+usmanmalik10@users.noreply.github.com>
2023-01-17 20:03:49 +0500

    grid view added

--- src/Products.js
+++ src/Products.js
@@ -3,15 +3,8 @@ import styled from 'styled-components';
 import FilterSection from './components/FilterSection';
 import Sort from './components/Sort';
 import ProductList from './components/ProductList';
-import { useFilterContext } from './context/filterContext';
 
 const Products = () => {
-  const { filter_products } = useFilterContext();
-  console.log(
-    '🚀 ~ file: Products.js:10 ~ Products ~ filter_products',
-    filter_products
-  );
-
   return (
     <Wrapper>
       <div className="container grid grid-filter-column">
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import { useFilterContext } from '../context/filterContext';
+import GridView from './GridView';
 
 const ProductList = () => {
+  const { filter_products, setGridView } = useFilterContext();
+
+  if (setGridView) {
+    return <GridView products={filter_products} />;
+  }
+
+  // if (setGridView === false) {
+  //   return <ListView products={filter_products} />;
+  // }
+
   return <div>ProductList</div>;
 };
 
