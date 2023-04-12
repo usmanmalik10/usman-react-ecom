e790d27ae821dfb046e1ed3eebd2309110bb4aa2
usmanmalik10 <usmanmaliksahib2611@gmail.com>
2023-04-13 03:05:18 +0500

    dropdown list added

--- src/context/filterContext.js
+++ src/context/filterContext.js
@@ -7,7 +7,8 @@ const FilterContext = createContext();
 const initialState = {
   filter_products: [],
   all_products: [],
-  grid_view: false,
+  grid_view: true,
+  sorting_value: 'lowest',
 };
 
 export const FilterContextProvider = ({ children }) => {
@@ -17,14 +18,31 @@ export const FilterContextProvider = ({ children }) => {
 
   //   to set the grid view
   const setGridView = () => {
-    return dispatch({ type: 'SET_GRIDVIEW' });
+    return dispatch({ type: 'SET_GRID_VIEW' });
   };
+
+  // to set the list view
+  const setListView = () => {
+    return dispatch({ type: 'SET_LIST_VIEW' });
+  };
+
+  // Sorting Function
+  const sorting = () => {
+    dispatch({ type: 'GET_SORT_VALUE' });
+  };
+  // to sort the products
+  useEffect(() => {
+    dispatch({ type: 'SORTING_PRODUCTS', payload: products });
+  }, [state.sorting_value]);
+
   useEffect(() => {
     dispatch({ type: 'LOAD_FILTER_PRODUCTS', payload: products });
   }, [products]);
 
   return (
-    <FilterContext.Provider value={{ ...state, setGridView }}>
+    <FilterContext.Provider
+      value={{ ...state, setGridView, setListView, sorting }}
+    >
       {children}
     </FilterContext.Provider>
   );
