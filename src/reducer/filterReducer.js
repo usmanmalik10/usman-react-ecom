25d5c068f66966412879ca1d0929cfa7eb72ecc7
usmanmalik10 <usmanmaliksahib2611@gmail.com>
2023-05-19 23:02:28 +0500

    search, category and company wise filter apply

--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -73,11 +73,21 @@ const filterReducer = (state, action) => {
     case 'FILTER_PRODUCTS':
       let { all_products } = state;
       let tempFilterProduct = [...all_products];
-      const { text } = state.filters;
+      const { text, category, company } = state.filters;
 
       if (text) {
         tempFilterProduct = tempFilterProduct.filter((curElem) => {
           return curElem.name.toLowerCase().includes(text);
+        });
+      }
+      if (category !== 'all') {
+        tempFilterProduct = tempFilterProduct.filter((curElem) => {
+          return curElem.category === category;
+        });
+      }
+      if (company !== 'all') {
+        tempFilterProduct = tempFilterProduct.filter((curElem) => {
+          return curElem.company.toLowerCase() === company.toLowerCase();
         });
       }
 
