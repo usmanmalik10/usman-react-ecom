b6f094f4f5565634dc06f3012ed57abf0129465d
usmanmalik10 <usmanmaliksahib2611@gmail.com>
2023-06-01 22:47:25 +0500

    remove cart

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -9,14 +9,20 @@ const initialState = {
   total_amount: '',
   shipping_fee: 50000,
 };
+
 const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const addToCart = (id, color, amount, product) => {
     dispatch({ type: 'ADD_TO_CART', payload: { id, color, amount, product } });
   };
+
+  const removeItem = (id) => {
+    dispatch({ type: 'REMOVE_ITEM', payload: id });
+  };
+
   return (
-    <CartContext.Provider value={{ ...state, addToCart }}>
+    <CartContext.Provider value={{ ...state, addToCart, removeItem }}>
       {children}
     </CartContext.Provider>
   );
@@ -25,4 +31,5 @@ const CartProvider = ({ children }) => {
 const useCartContext = () => {
   return useContext(CartContext);
 };
+
 export { CartProvider, useCartContext };
