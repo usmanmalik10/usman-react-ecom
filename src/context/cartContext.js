3c783859219b53a412f6ad48967c22c1e502acb0
usmanmalik10 <usmanmaliksahib2611@gmail.com>
2023-06-08 20:28:06 +0500

    commit to 50

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -1,10 +1,20 @@
-import { createContext, useContext, useReducer } from 'react';
+import { createContext, useContext, useEffect, useReducer } from 'react';
 import reducer from '../reducer/cartReducer';
 
 const CartContext = createContext();
 
+const getLocalCartData = () => {
+  let localCartData = localStorage.getItem('thapaCart');
+  if (localCartData === []) {
+    return [];
+  } else {
+    return JSON.parse(localCartData);
+  }
+};
+
 const initialState = {
-  cart: [],
+  // cart: [],
+  cart: getLocalCartData(),
   total_item: '',
   total_amount: '',
   shipping_fee: 50000,
@@ -21,8 +31,22 @@ const CartProvider = ({ children }) => {
     dispatch({ type: 'REMOVE_ITEM', payload: id });
   };
 
+  // to remove the individual item form cart
+  const clearCart = () => {
+    dispatch({ type: 'CLEAR_CART' });
+  };
+
+  //  to add the data in localstore
+  // get vs set
+
+  useEffect(() => {
+    localStorage.setItem('thapaCart', JSON.stringify(state.cart));
+  }, [state.cart]);
+
   return (
-    <CartContext.Provider value={{ ...state, addToCart, removeItem }}>
+    <CartContext.Provider
+      value={{ ...state, addToCart, removeItem, clearCart }}
+    >
       {children}
     </CartContext.Provider>
   );
